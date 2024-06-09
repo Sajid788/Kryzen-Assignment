b534b127ad7ee288e44308f76b3594cd6bb4fb51
Md Sajid Ansari <mdsajidalam8340@gmail.com>
2024-06-09 15:00:41 +0530

    middleware done

--- backend/routes/product_routes.js
+++ backend/routes/product_routes.js
@@ -6,11 +6,13 @@ const {
   getProducts,
 } = require("../controller/product_controller");
 
+const authorization = require('../middleware/authorization')
+
 const productRouter = express.Router();
 
-productRouter.post("/add", addProduct);
-productRouter.put("/edit/:id", editProduct);
-productRouter.delete("/delete/:id", deleteProduct);
+productRouter.post("/add",authorization, addProduct);
+productRouter.put("/edit/:id",authorization, editProduct);
+productRouter.delete("/delete/:id",authorization, deleteProduct);
 productRouter.get("/", getProducts);
 
 module.exports = { productRouter };
