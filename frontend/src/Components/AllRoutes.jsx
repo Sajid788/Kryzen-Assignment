9429a3c223bb7895dc6bfef75e39db591e29d63f
Md Sajid Ansari <mdsajidalam8340@gmail.com>
2024-06-11 06:26:31 +0530

    Privates routes implimented

--- frontend/src/Components/AllRoutes.jsx
+++ frontend/src/Components/AllRoutes.jsx
@@ -3,12 +3,13 @@ import { Route, Routes } from 'react-router-dom'
 import Signup from '../Page/Signup'
 import Login from '../Page/Login'
 import ProductPage from '../Page/ProductPage'
+import PrivateRoutes from './PrivateRoutes'
 const AllRoutes = () => {
   return (
    <Routes>
     <Route path="/" element={<Signup/>} />
     <Route path="/login" element={<Login />} />
-    <Route path="/product" element={<ProductPage />} />
+    <Route path="/product" element={<PrivateRoutes><ProductPage /></PrivateRoutes>} />
    </Routes>
   )
 }
